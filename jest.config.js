3411a89241866b0837b62333eefd36981f4fe1e2
lucas vinicius <lucassilved@gmail.com>
2023-12-24 19:54:39 -0300

    initial config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ['<rootDir>/tests'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+}
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { Hello } from '@/example/example'
+
+function main(): void {
+  console.log(`${Hello}, e ai`)
+}
+main()
