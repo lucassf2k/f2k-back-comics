a1512d19391763390594f9ff9e38a60b2f399ffa
lucas vinicius <lucassilved@gmail.com>
2023-12-25 18:53:58 -0300

    feat: add domain category tests

---
+++ tests/domain/category/Category.spec.ts
@@ -0,0 +1,24 @@
+import { Category } from '@/domain/category/Category'
+
+describe('Category Test', () => {
+  test('should be create a new category', () => {
+    const sut = Category.create('Fantasia')
+    expect(sut.id).toBeTruthy()
+    expect(sut.name).toBe('Fantasia')
+    expect(sut.createdAt).toBeTruthy()
+  })
+
+  test('should not create a new category with a name that is made up of numbers', () => {
+    const nameInvalid = '1235'
+    expect(() => Category.create(nameInvalid)).toThrow(
+      `Invalid param: ${nameInvalid}`,
+    )
+  })
+
+  test('You should not create a new category with a name that is made up of characters and symbols', () => {
+    const nameInvalid = 'Fantasi@'
+    expect(() => Category.create(nameInvalid)).toThrow(
+      `Invalid param: ${nameInvalid}`,
+    )
+  })
+})
