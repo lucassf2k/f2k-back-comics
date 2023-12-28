b595e6c6ed43987d21d5694c35f3f547e81de9ef
lucas vinicius <lucassilved@gmail.com>
2023-12-28 10:18:33 -0300

    feat: add error when reading that the name field is empty

--- src/infrastructure/express/controllers/genre/CreateGenreController.ts
+++ src/infrastructure/express/controllers/genre/CreateGenreController.ts
@@ -5,12 +5,15 @@ import {
 } from '@/domain/usecases/genre/ICreateGenre'
 import { HttpStatusCodes } from '@/application/enums/HttpStatusCodes'
 import { errorHandler } from '../../middlewares/errorHandler'
+import { InvalidParameterError } from '@/domain/errors/InvalidParameterError'
 
 export class CreateGenreController {
   constructor(private readonly createGenre: ICreateGenre) {}
 
   async handle(request: Request, response: Response): Promise<Response> {
     try {
+      if (!request.body.name)
+        throw new InvalidParameterError('Name field is required')
       const input: CreateGenreInput = request.body
       const { url } = await this.createGenre.execute(input)
       return response
