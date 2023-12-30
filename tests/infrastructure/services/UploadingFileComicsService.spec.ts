85001fc3ff356880d57adf718d729028ac4dd406
lucas vinicius <lucassilved@gmail.com>
2023-12-30 15:33:21 -0300

    feat: add resolve of node:path to filePath Uploading file

--- tests/infrastructure/services/UploadingFileComicsService.spec.ts
+++ tests/infrastructure/services/UploadingFileComicsService.spec.ts
@@ -1,9 +1,10 @@
-import { readFileSync } from 'fs'
+import { resolve } from 'node:path'
+import { readFileSync } from 'node:fs'
 import { UploadingFileComicsService } from '@/infrastructure/services/UploadingFileComicsService'
 
 describe('UploadingFileComicsService Test', () => {
   test('should be create a diectory', () => {
-    const filePath = './comics/6-2.pdf'
+    const filePath = resolve(__dirname, '..', '..', '..', 'comics', '6-2.pdf')
     const file: { originalname: string; buffer: Buffer } = {
       originalname: 'vamoslá.pdf',
       buffer: readFileSync(filePath),
