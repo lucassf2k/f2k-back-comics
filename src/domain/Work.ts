dfbbbedbfcfbbf3ba81ba5c95ef880ba2ebbb2ee
lucas vinicius <lucassilved@gmail.com>
2023-12-31 23:31:20 -0300

    refactor: work domain

--- src/domain/Work.ts
+++ src/domain/Work.ts
@@ -1,13 +1,41 @@
 import { IdGenerateService } from '@/infrastructure/services/IdGenerateService'
 
-export class Work {
-  readonly id: string
+export type WorkProps = {
+  title: string
+  releaseDate: Date
+  path: string
+}
 
-  constructor(
-    readonly title: string,
-    readonly releaseDate: Date,
-    readonly path: string,
-  ) {
-    if (!this.id) this.id = IdGenerateService.ULID()
+export class Work {
+  private readonly _id: string
+  private readonly props: Required<WorkProps>
+
+  constructor(props: WorkProps) {
+    if (!this._id) this._id = IdGenerateService.ULID()
+    this.props = props
+  }
+
+  updateTitle(title: string): void {
+    if (title) this.props.title = title
+  }
+
+  updatePath(path: string): void {
+    if (path) this.props.path = path
+  }
+
+  get title(): string {
+    return this.props.title
+  }
+
+  get releaseDate(): Date {
+    return this.props.releaseDate
+  }
+
+  get path(): string {
+    return this.props.path
+  }
+
+  get id(): string {
+    return this._id
   }
 }
--- tests/domain/Work.spec.ts
+++ tests/domain/Work.spec.ts
@@ -1,15 +1,42 @@
-import { Work } from '@/domain/Work'
+import { Work, WorkProps } from '@/domain/Work'
 
 describe('Work Test', () => {
   test('should be create work', () => {
-    const sut = new Work(
-      'Crônicas nas Estrelas',
-      new Date(),
-      'comics/154546/15487.pdf',
-    )
+    const workProps: WorkProps = {
+      title: 'Crônicas nas Estrelas',
+      releaseDate: new Date(),
+      path: 'comics/154546/15487.pdf',
+    }
+    const sut = new Work(workProps)
     expect(sut).toHaveProperty('id')
     expect(sut).toHaveProperty('title')
     expect(sut).toHaveProperty('releaseDate')
     expect(sut).toHaveProperty('path')
+  })
+
+  test('should be update title and path', () => {
+    const workProps: WorkProps = {
+      title: 'Crônicas nas Estrelas',
+      releaseDate: new Date(),
+      path: 'comics/154546/15487.pdf',
+    }
+    const sut = new Work(workProps)
+    sut.updateTitle('Estrela do Norte')
+    sut.updatePath('asd.pdf')
+    expect(sut.title).toStrictEqual('Estrela do Norte')
+    expect(sut.path).toStrictEqual('asd.pdf')
+  })
+
+  test('should be not update title and path when parameters empty', () => {
+    const workProps: WorkProps = {
+      title: 'Crônicas nas Estrelas',
+      releaseDate: new Date(),
+      path: 'comics/154546/15487.pdf',
+    }
+    const sut = new Work(workProps)
+    sut.updateTitle('')
+    sut.updatePath('')
+    expect(sut.title).toStrictEqual('Crônicas nas Estrelas')
+    expect(sut.path).toStrictEqual('comics/154546/15487.pdf')
   })
 })
