bc09bb7af4dffdba3d4cc0ed6b0fbd95b652962b
lucas vinicius <lucassilved@gmail.com>
2024-01-05 17:27:01 -0300

    feat: using native location of response

--- src/infrastructure/express/controllers/CreateComicController.ts
+++ src/infrastructure/express/controllers/CreateComicController.ts
@@ -12,7 +12,7 @@ export class CreateComicController {
     try {
       const input: CreateComicInput = request.body
       const output = await this.createComic.execute(input)
-      return response.setHeader('location', output.location)
+      return response.location(output.location)
     } catch (error) {
       errorHandler(error, request, response)
     }
